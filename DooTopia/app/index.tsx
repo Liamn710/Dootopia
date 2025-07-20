48b5dd2551c6d7a33bf76baf3335791e7a789afe
Maor Shoval <maorshoval@gmail.com>
2025-07-20 18:05:15 +0300

    login page frontend

--- DooTopia/app/index.tsx
+++ DooTopia/app/index.tsx
@@ -1,15 +1,94 @@
-import { Text, View } from "react-native";
+
+
+import { useState } from 'react';
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+
 
 export default function Index() {
+  const [email, setEmail]=useState("");//state variable for setting an email
+  const [password , setPassword]=useState("");// state variable for the password
+
+  //login handling 
+  const handleLogin = ()=> {
+   if (!email || !password)
+    {
+      Alert.alert("Error","Please fiil in all fields");
+      return;
+    }
+  console.log("Login attempted with",email,password);
+  }
+
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>hello there</Text>
+    <View style={styles.container}>
+
+      <Text style = {styles.title}> Welcome </Text>
+
+      <View style = {styles.form}>
+        <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        />
+
+        <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        />
+
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Log In</Text>
+      </TouchableOpacity>
+
+
+      </View>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#D6ECF2", 
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 30,
+    color: "#333",
+  },
+  form: {
+    width: "100%",
+    maxWidth: 300,
+  },
+  input: {
+    backgroundColor: "#9DBCC3",
+    paddingHorizontal: 15,
+    paddingVertical: 12,
+    borderRadius: 8,
+    marginBottom: 15,
+    borderWidth: 1,
+    borderColor: "#ddd",
+  },
+  button: {
+    backgroundColor: "#5A8A93",
+    paddingVertical: 12,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
